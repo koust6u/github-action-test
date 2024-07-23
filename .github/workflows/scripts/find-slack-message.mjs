cee95fa6f72eb275ecb7222f6001dc3c2a0e7fff
김민종 <koust6u@pusan.ac.kr>
2024-07-23 13:11:07 +0900

    Create find-slack-message.mjs

---
+++ .github/workflows/scripts/find-slack-message.mjs
@@ -0,0 +1,25 @@
+import { Octokit } from "@octokit/rest";
+import { setOutput, setFailed } from "@actions/core";
+import { context } from "@actions/github";
+
+(async () => {
+  try {
+    const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+    const { data: comments } = await octokit.issues.listComments({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: context.payload.pull_request.number
+    });
+
+    const slackMessage = comments.find(comment => comment.body.includes(context.payload.pull_request.html_url));
+
+    if (slackMessage) {
+      setOutput('result', slackMessage.id);
+    } else {
+      setOutput('result', '');
+    }
+  } catch (error) {
+    setFailed(error.message);
+  }
+})();
